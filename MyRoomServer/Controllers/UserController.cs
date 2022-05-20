2376158af8de667ead6439d5db4e46a00eb11f6e
saicem <saicem@qq.com>
2022-05-20 10:18:40 +0800

    feat: the CRUD of database entities.

--- MyRoomServer/Controllers/UserController.cs
+++ MyRoomServer/Controllers/UserController.cs
@@ -1,41 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿//using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+//// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace MyRoomServer.Controllers
-{
-    [Route("user")]
-    [ApiController]
-    public class UserController : ControllerBase
-    {
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+//namespace MyRoomServer.Controllers
+//{
+//    [Route("user")]
+//    [ApiController]
+//    public class UserController : ControllerBase
+//    {
+//        [HttpGet]
+//        public IEnumerable<string> Get()
+//        {
+//            return new string[] { "value1", "value2" };
+//        }
 
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
+//        [HttpGet("{id}")]
+//        public string Get(int id)
+//        {
+//            return "value";
+//        }
 
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
+//        [HttpPost]
+//        public void Post([FromBody] string value)
+//        {
 
-        }
+//        }
 
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
+//        [HttpPut("{id}")]
+//        public void Put(int id, [FromBody] string value)
+//        {
 
-        }
+//        }
 
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+//        [HttpDelete("{id}")]
+//        public void Delete(int id)
+//        {
 
-        }
-    }
-}
+//        }
+//    }
+//}
