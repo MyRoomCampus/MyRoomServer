30eab2d86cd68c0b0273c3f1517a922321e53314
saicem <saicem@qq.com>
2022-05-20 10:36:21 +0800

    fix: the forget file for last commit

---
+++ MyRoomServer/Extentions/ControllerBaseExtention.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace MyRoomServer.Extentions
+{
+    public static class ControllerBaseExtention
+    {
+        public static string GetUserId(this ControllerBase controller)
+        {
+            var userId = controller.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (userId == null)
+            {
+                throw new NullReferenceException("Couldn't found user's id claim.");
+            }
+            return userId;
+        }
+    }
+}
