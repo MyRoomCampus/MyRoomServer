f4b4f9f9471541d9542ef4ef56fbaf0bfc757cf3
saicem <saicem@qq.com>
2022-05-22 09:28:15 +0800

    feat: add ping pong api for production test

---
+++ MyRoomServer/Controllers/CommonController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyRoomServer.Entities;
+using MyRoomServer.Extentions;
+using MyRoomServer.Models;
+using MyRoomServer.Services;
+using System.ComponentModel.DataAnnotations;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace MyRoomServer.Controllers
+{
+    [ApiController]
+    [Route("common")]
+    public class CommonController : ControllerBase
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult Ping()
+        {
+            return Ok("pong");
+        }
+    }
+}
