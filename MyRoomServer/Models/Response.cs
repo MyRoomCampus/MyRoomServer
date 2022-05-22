117ebe5e2e6b9debad9225c5d8c9385aec215929
saicem <saicem@qq.com>
2022-05-22 20:57:57 +0800

    fix: logging level set, missing response model

---
+++ MyRoomServer/Models/Response.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MyRoomServer.Models
+{
+    public class ApiRes
+    {
+        public ApiRes(string msg)
+        {
+            Msg = msg;
+        }
+
+        public ApiRes(string msg, object data)
+        {
+            Msg = msg;
+            Data = data;
+        }
+
+        public string Msg { get; set; } = null!;
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public object Data { get; set; } = null!;
+    }
+}
