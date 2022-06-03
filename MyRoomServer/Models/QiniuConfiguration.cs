486e7326d8ebd767692895ed6ecab4a56fc2de35
saicem <saicem@qq.com>
2022-06-03 19:35:41 +0800

    feat: add qiniu object storage

---
+++ MyRoomServer/Models/QiniuConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace MyRoomServer.Models
+{
+    public class QiniuConfiguration
+    {
+        public string AccessKey { get; set; } = null!;
+
+        public string SecretKey { get; set; } = null!;
+        
+        public string Bucket { get; set; } = null!;
+    }
+}
