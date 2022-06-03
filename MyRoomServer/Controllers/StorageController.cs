f2592000988989908f2a4eafaabcbbb64a1a0e15
saicem <56228142+saicem@users.noreply.github.com>
2022-06-03 19:41:32 +0800

    Add qiniu object storage support. (#36)
    * feat: add qiniu object storage
    
    * build: update environments

---
+++ MyRoomServer/Controllers/StorageController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyRoomServer.Models;
+using Qiniu.Storage;
+using Qiniu.Util;
+
+namespace MyRoomServer.Controllers
+{
+    [Route("storage")]
+    [ApiController]
+    public class StorageController : ControllerBase
+    {
+        private readonly QiniuConfiguration config;
+
+        public StorageController(IConfiguration configuration)
+        {
+            config = new QiniuConfiguration();
+            configuration.GetSection("Qiniu").Bind(config);
+        }
+
+        [HttpGet]
+        [Authorize(Policy = IdentityPolicyNames.CommonUser)]
+        public IActionResult GetToken()
+        {
+            var mac = new Mac(config.AccessKey, config.SecretKey);
+            var putPolicy = new PutPolicy
+            {
+                Scope = config.Bucket
+            };
+
+            var token = Auth.CreateUploadToken(mac, putPolicy.ToJsonString());
+            return Ok(new ApiRes("获取 token 成功", token));
+        }
+    }
+}
